ace70d009c0eb273b2dc888b39d15f65e173842c
Aishwarya0700 <135342021+Aishwarya0700@users.noreply.github.com>
2023-09-07 12:08:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var http = require('http');
 //create a server object:
 http.createServer(function (req, res) {
   res.write('** Hey my app was deployed :) **'); //write a response to the client
+  res.write(' Welcome to CI/CD ')
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
